894176a3493b34b92d9f27e12aaec7b60b9c0a38
Tilyeujan <tikucsms@gmail.com>
2017-10-12 14:53:11 +0800

    Deploy aldaa zassan

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const proxy = require('http-proxy-middleware');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || '5000';
 
 app.set('port', port);
 app.use(express.static(__dirname + '/dist'));
-
-var apiProxy = proxy('/assets/json', {'target':'http://www.arp2.herokuapp.com', "secure": false,});
 
 app.get('/[^\.]+$', function (req, res) {
     res.set('Content-Type', 'text/html').sendFile(path.join(__dirname + '/dist/index.html'));
