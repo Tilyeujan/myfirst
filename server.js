042dd8ceec0d6d4f693749b89e080431a0540a15
Tilyeujan <tikucsms@gmail.com>
2017-10-12 10:49:22 +0800

    Deploy aldaa

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT | '8080';
 
+app.set('port', port);
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.Port || 8080);
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/[^\.]+$', function (req, res) {
+    res.set('Content-Type', 'text/html').sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-console.log('Console listening!');
+app.listen(app.get('port'), function(){
+    console.log("Node app is running at localhost:"+ app.get('port'));
+});
